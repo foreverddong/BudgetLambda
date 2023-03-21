811c4b67d62f13010267ad47764004055afa2a7f
Donglin <foreverddong@icloud.com>
2023-03-20 21:47:54 -0700

    run sink configuration before starting listening

--- BudgetSource/StdoutSink/Program.cs
+++ BudgetSource/StdoutSink/Program.cs
@@ -1,3 +1,8 @@
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,10 +10,35 @@ builder.Logging.AddJsonConsole();
 
 
 var app = builder.Build();
-
+ConfigureSinkStdOut();
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+void ConfigureSinkStdOut()
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var configuration = app.Services.GetRequiredService<IConfiguration>();
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    var factory = new ConnectionFactory
+    {
+        HostName = configuration.GetValue<string>("RabbitMQ:Hostname"),
+        UserName = configuration.GetValue<string>("RabbitMQ:Username"),
+        Password = configuration.GetValue<string>("RabbitMQ:Password"),
+        VirtualHost = configuration.GetValue<string>("RabbitMQ:VirtualHost"),
+    };
+    var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+
+    var queueName = configuration.GetValue<string>("Pipeline:Queue");
+    var exchangeName = configuration.GetValue<string>("Pipeline:Exchange");
+    var routingKey = configuration.GetValue<string>("Pipeline:Key");
+    channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+    channel.QueueBind(queueName, exchangeName, routingKey);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (ch, ea) =>
+    {
+        var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+        logger.LogInformation($"{body}");
+        channel.BasicAck(ea.DeliveryTag, false);
+    };
+    channel.BasicConsume(queueName, true, consumer);
 }
