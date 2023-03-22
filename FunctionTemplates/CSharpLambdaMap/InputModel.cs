606b5192ae84a0cc1ba7b3c840fa88fec67280eb
Donglin <foreverddong@icloud.com>
2023-03-22 13:29:40 -0700

    complete the template, making it intentially non-compiling for now.

---
+++ FunctionTemplates/CSharpLambdaMap/InputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpFunction
+{
+    public class InputModel
+    {
+        public string Info => "Default input model, users should replace this.";
+        intentially make this non-compiling :(
+        public int Number { get; set; }
+    }
+}
---
+++ FunctionTemplates/CSharpLambdaMap/OutputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpFunction
+{
+    public class OutputModel
+    {
+        public string Info => "Default Output model, users should replace this.";
+
+        public string Words { get; set; }
+    }
+}
