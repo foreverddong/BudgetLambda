18ae0ef18fedbcef87b16229c34161d897626bb1
Donglin Xu <xu.dong@northeastern.edu>
2023-03-22 20:48:50 +0000

    fix watchdog in function image building stages

--- FunctionTemplates/CSharpLambdaMap/InputModel.cs
+++ FunctionTemplates/CSharpLambdaMap/InputModel.cs
@@ -3,7 +3,6 @@
     public class InputModel
     {
         public string Info => "Default input model, users should replace this.";
-        intentially make this non-compiling :(
         public int Number { get; set; }
     }
 }
