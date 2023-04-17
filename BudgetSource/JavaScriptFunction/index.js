36a108208e5f44a538b110009a1d63b200806d26
Yiheng Niu <niuyiheng@Ians-MacBook-Pro.local>
2023-04-16 21:34:23 -0700

    feat: integrate pg in js function

--- BudgetSource/JavaScriptFunction/index.js
+++ BudgetSource/JavaScriptFunction/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const amqp = require("amqplib")
-const env_config = require("dotenv").config()
+require("dotenv").config()
 const handler = require("./handler")
 
 const app = express()
